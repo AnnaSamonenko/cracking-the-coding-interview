5aee7c0e56804285f5756a402a7703a1ccecb01a
Anna Samonenko <annsamonenko97@gmail.com>
2019-07-05 07:12:21 +0300

    ZeroMatrix

--- src/com/gmail/samonenko/arrays_and_strings/ZeroMatrix.java
+++ src/com/gmail/samonenko/arrays_and_strings/ZeroMatrix.java
@@ -1,8 +1,11 @@
 package com.gmail.samonenko.arrays_and_strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ZeroMatrix {
 
-    int[][] matrix = {
+    static int[][] matrix = {
             {1, 0, 1, 1, 1},
             {1, 1, 1, 1, 1},
             {1, 1, 1, 0, 1},
@@ -12,10 +15,42 @@ public class ZeroMatrix {
     };
 
     public static void main(String[] args) {
+        zeroMatrix(matrix);
+        sout(matrix);
+
     }
 
-    static int[][] zeroMatrix(){
-        return null;
+    static int[][] zeroMatrix(int[][] matrix) {
+        List<Integer> rows = new ArrayList<>();
+        List<Integer> columns = new ArrayList<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    columns.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (rows.contains(i) || columns.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void sout(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
